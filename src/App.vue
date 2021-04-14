61915ddabdc81458cbc5bb81bc893e7426e80213
Lionatic <ahmadhakim404@gmail.com>
2021-04-14 15:39:49 +0700

    styling navbar

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link :to="{ name: 'Product' }">Product</router-link> |
-      <router-link :to="{ name: 'Cart' }">Cart</router-link>
-    </div>
+    <Navbar />
     <router-view/>
   </div>
 </template>
+
+<script>
+import Navbar from './components/Navbar'
+export default {
+  name: 'app',
+  components: {
+    Navbar
+  }
+}
+</script>
 
 <style>
   * {
--- src/views/product/Index.vue
+++ src/views/product/Index.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-      <h1>Product Page</h1>
       <div id="page-wrap">
         <div class="grid-wrap">
           <div 
