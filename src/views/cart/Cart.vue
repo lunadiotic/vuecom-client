cd90fc0948de01ea952475ed1c5f2aa2e57348bc
Lionatic <ahmadhakim404@gmail.com>
2021-04-14 15:28:33 +0700

    styling shopping cart

--- src/views/cart/Cart.vue
+++ src/views/cart/Cart.vue
@@ -1,20 +1,84 @@
 <template>
   <div>
-      <h1>Chart Page</h1>
+      <div id="page-wrap">
+          <h1>Shopping Cart</h1>
+          <div
+            v-for="item in cartItems"
+            :key="item.id"
+            class="product-container"
+          >
+             <img :src="item.imageUrl" alt="" class="product-image">
+             <div class="details-wrap">
+                 <h3>{{ item.name }}</h3>
+                 <p>Rp{{ item.price }}</p>
+             </div>
+             <button class="remove-button">Remove</button>
+          </div>
+          <h3 id="total-price">Total: Rp{{ totalPrice }}</h3>
+          <button id="checkout-button">Checkout</button>
+      </div>
   </div>
 </template>
 
 <script>
+import { cartItems } from '../../fake-data'
+
 export default {
-    name: 'chart-index'
+    name: 'chart-index',
+    data() {
+        return {
+            cartItems
+        }
+    },
+    computed: {
+        totalPrice() {
+            return this.cartItems.reduce(
+                (sum, item) => sum + Number(item.price),
+                0
+            )
+        }
+    }
 }
 </script>
 
 <style scoped>
     h1 {
-        border-bottom: 1px solid black;
-        margin: 0;
-        margin-top: 16px;
-        padding: 16px;
-    }
+    border-bottom: 1px solid #41B883;
+    margin: 0;
+    margin-top: 16px;
+    padding: 16px;
+  }
+
+  #total-price {
+    padding: 16px;
+    text-align: right;
+  }
+
+  #checkout-button {
+    width: 100%;
+  }
+
+  .product-container {
+    align-content: 'center';
+    border-bottom: 1px solid #ddd;
+    display: flex;
+    padding: 16px;
+    width: 100%;
+  }
+
+  .product-image {
+    flex: 1;
+    height: 100px;
+    max-width: 100px;
+  }
+
+  .details-wrap {
+    padding: 0 16px;
+    flex: 3;
+  }
+
+  .remove-button {
+    flex: 1;
+    margin: auto;
+  }
 </style>
